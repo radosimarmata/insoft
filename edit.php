30698a3e60ed6d26b1ec2860585765e3cbcd2bc1
radomen <simarmatarado@gmail.com>
2021-03-29 23:22:48 +0700

    Pakai bootsrap

--- edit.php
+++ edit.php
@@ -7,12 +7,25 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
+    
     <title>Insoft CRUD</title>
 </head>
 <body>
-    <a href="index.php"><h1>INSOFT</h1></a>
-    <h3>EDIT Book</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">INSOFT</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            </div>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+    <h5>EDIT Book</h5>
 
     <?php
     include 'connect.php';
@@ -44,5 +57,6 @@
         <?php
     }
     ?>
+    </div>
 </body>
 </html>
